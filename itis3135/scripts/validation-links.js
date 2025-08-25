08508ba831d46e0737fa6208be41a76aa00cb267
jblandin-art <jblandin@charlotte.edu>
2025-08-24 23:33:21 -0400

    scripts made external need directory fixed

---
+++ itis3135/scripts/validation-links.js
@@ -0,0 +1,2 @@
+document.getElementById("validation-button-html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+document.getElementById("validation-button-css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
